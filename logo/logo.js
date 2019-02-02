6958077804053ec1ad4fb0c46d773c66fa7173f0
stargamer3 <juliengrijalva@gmail.com>
2019-02-01 17:32:06 -0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -4,13 +4,16 @@ let pc = [255,255,255];
 let input, button;
 let fc = [255,255,255];
 let cmd = '';
-let name = '';
 //let inp = createInput();
+function greet(){
+  const name = input.value();
+  console.log(name);
+  input.value('');
+}
 function setup(){
   createCanvas(601,601);
   input = createInput();
   input.position(20, 65);
-
   button = createButton('submit');
   button.position(input.x + input.width, 65);
   button.mousePressed(greet);
@@ -21,9 +24,4 @@ function draw(){
   strokeWeight(1);
   fill(sc[0],sc[1],sc[2]);
   rect(0,0,600,600);
-}
-function greet(){
-  //name = input.value();
-  //console.log(name);
-  //input.value('');
 }
