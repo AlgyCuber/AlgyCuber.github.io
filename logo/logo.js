cae9788556aa1d99226f4e26cde283e46abf698e
stargamer3 <juliengrijalva@gmail.com>
2019-02-01 21:51:42 -0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -1,4 +1,4 @@
-console.log(38);
+console.log(39);
 let sc = [255,0,0];
 let pc = [255,255,255];
 let input, button;
@@ -30,7 +30,7 @@ function draw(){
   background(255);
   push();
   translate(x+10,y+10);
-  rotate(360);
+  rotate(radians(360));
   dtb();
   pop();
 }
