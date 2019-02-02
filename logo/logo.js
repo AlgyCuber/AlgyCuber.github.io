1384e96bbda02d351080f386ea31d8a1dd4718ca
stargamer3 <juliengrijalva@gmail.com>
2019-02-01 21:34:53 -0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -15,7 +15,7 @@ function dtb(){
   stroke(0);
   strokeWeight(1);
   fill(sc[0],sc[1],sc[2]);
-  rect(0,0,20,20);
+  rect(10,10,20,20);
 } //draw turtle
 function setup(){
   createCanvas(601,601);
@@ -29,7 +29,7 @@ function setup(){
 function draw(){
   background(255);
   push();
-  translate(x,y);
+  translate(x-10,y-10);
   rotate(45);
   dtb();
   pop();
