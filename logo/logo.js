7bae6a0c3f6d5b858cfdef29359bd61ce1cb1f7a
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-02-10 21:04:27 +0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -5,26 +5,11 @@ let input,button;
 let fc = [255,255,255];
 let cmd = '';
 let x = 300, y = 300, dir = 0;
-function greet(){
-  const name = input.value();
-  console.log(name);
-  input.value('');
-}
-function dtb(){
-  stroke(0);
-  strokeWeight(1);
-  fill(sc[0],sc[1],sc[2]);
-  rect(-10,-10,20,20);
-  fill(0);
-  rect(-10,-10,20,10);
-}
 //draw turtle
 function setup(){
   createCanvas(601,601);
   input = createInput();
   input.position(700, 65);
-  input.width = 400;
-  console.log(input.width);
   button = createButton('submit');
   button.position(input.x + input.width, 65);
   button.mousePressed(greet);
@@ -32,9 +17,4 @@ function setup(){
 }
 function draw(){
   background(255);
-  push();
-  translate(x+10,y+10);
-  rotate(radians(dir));
-  dtb();
-  pop();
 }
