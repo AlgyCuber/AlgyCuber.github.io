e3fb45aefb142401f40147f53d734c4c74537ee1
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-02-13 19:37:35 +0700

    Update idelab.js

--- idelab/idelab.js
+++ idelab/idelab.js
@@ -1,6 +1,7 @@
-console.log(1);
+console.log(2);
 function setup(){
   createCanvas(window.innerWidth,window.innerHeight);
+  defaultCanvas0.position = 'absolute';
 }
 function draw(){
   background(250);
