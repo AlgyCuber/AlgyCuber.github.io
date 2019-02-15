de4297c1a98bf89c8cfe4fb7c4a5021399d55857
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-02-15 21:12:32 +0700

    Update idelab.js

--- idelab/idelab.js
+++ idelab/idelab.js
@@ -1,9 +1,9 @@
-console.log(21);
+console.log(22);
 let dark = false;
 let w = window.innerWidth;
 let h = window.innerHeight;
-let t = 0; //tab : 0 = idle, 1 = lab
-let onMenu = false;
+let t = 0; //tab : 0 = idle, 1 = lab, 2 = games
+let abucks = 
 function setup(){
   createCanvas(w,h);
   defaultCanvas0.style.position = 'absolute';
@@ -11,30 +11,26 @@ function setup(){
   defaultCanvas0.style.top = '0px';
 }
 function draw(){
-  //background
-  background((dark)?40:250);
-  //top bar
-  noStroke();
-  fill(255,245,114);
-  rect(0,0,w,64);
-  //mouse events
-  if(mouseX>w-64 && mouseY<64){
-    fill(239,230,107);
-    rect(w-64,0,64,64);
-    document.body.style.cursor = 'pointer';
+  if(t==0){
+    background((dark)?40:250);
+    noStroke();
+    fill(255,245,114);
+    rect(0,0,w,64);
+    if(dark){
+      fill(128);
+      ellipse(32,32,32,32);
+    }
   }
-  else{document.body.style.cursor = 'default';}
-  //menu btn
-  fill(128);
-  rect(w-48,19,33,6);
-  rect(w-48,29,33,6);
-  rect(w-48,39,33,6);
-  //menu tab
-  if(onMenu){
-    fill(160);
-    rect(w-200,64,200,800);
-  }
+  else if(t==1)
+    background((dark)?40:250);
+    if(1){
+      ///
+    }
+    else{document.body.style.cursor = 'default';}
+    noStroke();
+    fill(255,245,114);
+    rect(0,0,w,64);
 }
 function mousePressed(){
-  if(mouseX>w-64 && mouseY<64){onMenu = (onMenu)?false:true;}
+  ///
 }
