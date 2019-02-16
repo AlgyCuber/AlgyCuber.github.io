2b4593f6a25846b15da49e827cdf4bba5f9f5e79
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-02-16 18:47:12 +0700

    Update idelab.js

--- idelab/idelab.js
+++ idelab/idelab.js
@@ -1,4 +1,4 @@
-console.log(39);
+console.log(40);
 let w = window.innerWidth;
 let h = window.innerHeight;
 let TAU;
@@ -40,7 +40,7 @@ function draw(){
     }
   }
   else if(t==1){
-    background((dark)?240:40);
+    background((dark)?60:240);
     noStroke();
     fill(255,245,114);
     rect(0,0,w,64);
@@ -64,6 +64,10 @@ function draw(){
         triangle(32+22*cos(θ),32+22*sin(θ),32+14*cos(θ)-4/sqrt(3)*sin(θ),32+14*sin(θ)+4/sqrt(3)*cos(θ),32+14*cos(θ)+8/sqrt(3)*sin(θ),32+14*sin(θ)-8/sqrt(3)*cos(θ));
       }
     }
+    fill(140,222,255);
+    rect((w-40)/3+10,74,(w-40)/3,h/2-42);
+    rect(2*(w-40)/3+10,74,(w-40)/3,h/2-42);
+    rect(3*(w-40)/3+10,74,(w-40)/3,h/2-42);
   }
 }
 function mousePressed(){
