c0ef57ecc7aee50088343c5a3d9937999daf9fe6
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-02-16 08:55:17 +0700

    Update idelab.js

--- idelab/idelab.js
+++ idelab/idelab.js
@@ -1,4 +1,4 @@
-console.log(37);
+console.log(38);
 let w = window.innerWidth;
 let h = window.innerHeight;
 let TAU;
@@ -21,7 +21,9 @@ function draw(){
     if(mouseX>0 && mouseX<64 && mouseY>0 && mouseY<64){
       fill(255,241,92);
       rect(0,0,64,64);
+      document.body.style.cursor = 'pointer';
     }
+    else{document.body.style.cursor = 'default';}
     if(dark){
       fill(128);
       ellipse(32,32,32,32);
@@ -42,6 +44,26 @@ function draw(){
     noStroke();
     fill(255,245,114);
     rect(0,0,w,64);
+    if(mouseX>0 && mouseX<64 && mouseY>0 && mouseY<64){
+      fill(255,241,92);
+      rect(0,0,64,64);
+      document.body.style.cursor = 'pointer';
+    }
+    else{document.body.style.cursor = 'default';}
+    if(dark){
+      fill(128);
+      ellipse(32,32,32,32);
+      if(mouseX>0 && mouseX<64 && mouseY>0 && mouseY<64){fill(255,241,92);}
+      else{fill(255,245,144);}
+      ellipse(48,32,32,32);
+    }
+    else{
+      fill(128);
+      ellipse(32,32,20,20);
+      for(let θ=0;θ<TAU;θ+=TAU/8){
+        triangle(32+22*cos(θ),32+22*sin(θ),32+14*cos(θ)-4/sqrt(3)*sin(θ),32+14*sin(θ)+4/sqrt(3)*cos(θ),32+14*cos(θ)+8/sqrt(3)*sin(θ),32+14*sin(θ)-8/sqrt(3)*cos(θ));
+      }
+    }
   }
 }
 function mousePressed(){
