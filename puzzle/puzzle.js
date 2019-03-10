cff141d9cc608252bb01767e6a833911718043d0
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-03-10 11:26:34 +0700

    Update puzzle.js

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,1 +1,10 @@
-
+console.log(0);
+function setup(){
+  createCanvas(600,600);
+  defaultCanvas0.style.position = 'absolute';
+  defaultCanvas0.style.top = (window.innerHeight-600)/2+'px';
+  defaultCanvas0.style.left = (window.innerWidth-600)/2+'px';
+}
+function draw(){
+  background(250);
+}
