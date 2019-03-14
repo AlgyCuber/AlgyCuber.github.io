23332915bde51ffbae46e72fbaa17cf3e6a0329f
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-03-14 21:35:17 +0700

    Update puzzle.js

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,10 +1,18 @@
-console.log(0);
+console.log(1);
+let mT = (window.innerHeight-600)/2; //margin top
+let mL = (window.innerWidth-600)/2; //margin left
+function inCircle(mx,my,x,y,r){
+  if(sqrt(pow(mx-x,2)+pow(my-y,2))<r){return true;}
+  else{return false;}
+}
 function setup(){
   createCanvas(600,600);
   defaultCanvas0.style.position = 'absolute';
-  defaultCanvas0.style.top = (window.innerHeight-600)/2+'px';
-  defaultCanvas0.style.left = (window.innerWidth-600)/2+'px';
+  defaultCanvas0.style.top = mT+'px';
+  defaultCanvas0.style.left = mL+'px';
 }
 function draw(){
   background(250);
+  fill();
+  ellipse
 }
