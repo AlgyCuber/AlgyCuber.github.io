32195417ddab65a2f1803c4ff34c657582ae502b
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-03-19 12:08:17 +0700

    Update puzzle.js

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,4 +1,4 @@
-console.log(3);
+console.log(4);
 let mT = (window.innerHeight-600)/2; //margin top
 let mL = (window.innerWidth-600)/2; //margin left
 function inCircle(mx,my,x,y,r){
@@ -17,5 +17,5 @@ function draw(){
   fill(185,244,66);
   ellipse(300,300,100,100);
   fill(242);
-  triangle(330,300,285,300-15*sqrt(3),285,15*sqrt(3));
+  triangle(330,300,285,300-15*sqrt(3),285,300+15*sqrt(3));
 }
