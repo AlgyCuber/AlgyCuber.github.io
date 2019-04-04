685855212112c7be71f9bbd82d8a977cf2ae6726
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-04-04 13:27:25 +0700

    Update puzzle.js

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -17,7 +17,7 @@ function setup(){
   defaultCanvas0.style.left = mL+'px';
   defaultCanvas0.style.border = '1px solid';
   textAlign(CENTER);
-  textSize(15);
+  textSize(30);
 }
 function draw(){
   background(250);
@@ -50,7 +50,8 @@ function draw(){
     if(inf.l1.light){fill(35,222,255);}
     else{fill(21,90,155);}
     ellipse(300,150,32,32);
-    text('Level 1',300,25);
+    fill(0);
+    text('Level 1',300,30);
     if(inf.l1.over){}
   }
 }
