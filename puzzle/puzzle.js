9acefff169145608b9a82245b9fe96807da6b6dd
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-04-04 12:15:19 +0700

    Update puzzle.js

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,9 +1,8 @@
-console.log(18);
 let mT = (window.innerHeight-600)/2; //margin top
 let mL = (window.innerWidth-600)/2; //margin left
 let s = 0; //screen
 let inf = {//info
-  w1l1:{light:false}
+  l1:{light:false,over:false}
 };
 function inCircle(mx,my,x,y,r){
   return (sqrt(pow(mx-x,2)+pow(my-y,2))<r);
@@ -17,6 +16,7 @@ function setup(){
   defaultCanvas0.style.top = mT+'px';
   defaultCanvas0.style.left = mL+'px';
   defaultCanvas0.style.border = '1px solid';
+  textAlign(CENTER);
 }
 function draw(){
   background(250);
@@ -26,6 +26,7 @@ function draw(){
   else{document.body.style.cursor = 'default';}
   //layout
   if(s==0){
+    noStroke();
     if(inCircle(mouseX,mouseY,300,300,50)){fill(164,216,58);}
     else{fill(185,244,66);}
     ellipse(300,300,100,100);
@@ -41,20 +42,25 @@ function draw(){
     else{fill(242,236,75);}
     rect(284,434,32,32);
     strokeWeight(3);
-    if(inf.w1l1.light){stroke(255,0,0);}
+    if(inf.l1.light){stroke(255,0,0);}
     else{stroke(0);}
     line(300,166,300,434);
     stroke(0);
     strokeWeight(1);
-    if(inf.w1l1.light){fill(35,222,255);}
+    if(inf.l1.light){fill(35,222,255);}
     else{fill(21,90,155);}
     ellipse(300,150,32,32);
+    text('Level 1',300,10);
+    if(inf.l1.over){}
   }
 }
 function mousePressed(){
   if(s==0 && inCircle(mouseX,mouseY,300,300,50)){s = 1;}
   else if(s==1 && inRect(mouseX,mouseY,284,434,32,32)){
-    if(inf.w1l1.light){inf.w1l1.light = false;}
-    else{inf.w1l1.light = true;}
+    if(inf.l1.light){inf.l1.light = false;}
+    else{
+      inf.l1.light = true;
+      inf.l1.over = true;
+    }
   }
 }
