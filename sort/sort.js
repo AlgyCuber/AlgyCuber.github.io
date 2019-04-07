6a34b197a4d4e56d7af2e49471094265f1e86329
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-04-07 10:36:16 +0700

    Update sort.js

--- sort/sort.js
+++ sort/sort.js
@@ -1,10 +1,14 @@
 let alg = 0; //0 = bubble
+let sel;
 function setup(){
   createCanvas(windowWidth,windowHeight);
   defaultCanvas0.style.position = 'absolute';
   defaultCanvas0.style.top = '0px';
   defaultCanvas0.style.left = '0px';
+  sel = createSelect();
+  sel.position(32,32);
+  sel.option('Bubble Sort');
 }
 function draw(){
-  
+  background(255);
 }
