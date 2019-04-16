aaedce560279236c029d2be9f01c0f55299fe23b
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-04-16 10:12:07 +0700

    Update sort.js

--- sort/sort.js
+++ sort/sort.js
@@ -17,9 +17,9 @@ function inCircle(x,y,d){
 }
 function draw(){
   background(255);
+  stroke(1)
   fill(220);
   rect(0,0,width,64);
-  noStroke();
   fill((inCircle(width-2,32,48))?220:255);
   ellipse(width/2-48,32,48,48);
   fill((inCircle(width+2,32,48))?220:255);
