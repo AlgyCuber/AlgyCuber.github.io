426774060afb490a4c49e8a7f7437b6064ea46dd
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-04-16 10:10:42 +0700

    Update sort.js

--- sort/sort.js
+++ sort/sort.js
@@ -9,8 +9,19 @@ function setup(){
   sel.position(32,24);
   sel.option('Bubble Sort');
 }
+function inRect(x,y,w,h){
+  return (mouseX>x && mouseX<x+w && mouseY>y && mouseY<y+h);
+}
+function inCircle(x,y,d){
+  return (sqrt(pow(mouseX-x,2)+pow(mouseY-y,2))<d/2);
+}
 function draw(){
   background(255);
   fill(220);
   rect(0,0,width,64);
+  noStroke();
+  fill((inCircle(width-2,32,48))?220:255);
+  ellipse(width/2-48,32,48,48);
+  fill((inCircle(width+2,32,48))?220:255);
+  ellipse(width/2+48,32,48,48);
 }
