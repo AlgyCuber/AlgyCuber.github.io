77d71abc761bed8587cc4d7214ba79d958ba6d2c
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-04-26 15:48:48 +0700

    Update sort.js

--- sort/sort.js
+++ sort/sort.js
@@ -47,7 +47,7 @@ function draw(){
   }
   fill(0);
   for(let i=0;i<numinp.length;i++){
-    if(![0,1,2,3,4,5,6,7,8,9].contains(numinp[i])){numinp.value(numval);}
+    if(numinp[i]!=0){numinp.value(numval);}
   }
 }
 function mousePressed(){
