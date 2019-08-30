b62d01159fbb0b7ab25343f3de79f5b1d3897d49
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-08-30 19:40:30 +0700

    Update ipbipona.js

--- ipbipona/ipbipona.js
+++ ipbipona/ipbipona.js
@@ -20,7 +20,7 @@ function draw(){
 	else if(tVal[tVal.length-1]=='B'){t.value = t.value.substring(0,t.value.length-1)+'ʙ';}
 	else if(tVal[tVal.length-1]=='P'){t.value = t.value.substring(0,t.value.length-1)+'ᴘ';}
 	else if(tVal[tVal.length-1]=='V'){t.value = t.value.substring(0,t.value.length-1)+'ʌ';}
-	else if(tVal[tVal.length-1]=='F'){t.value = t.value.substring(0,t.value.length-1)+'ɟ';}
+	else if(tVal[tVal.length-1]=='F'){t.value = t.value.substring(0,t.value.length-1)+'ꜰ';}
 	else if(tVal[tVal.length-1]=='Z'){t.value = t.value.substring(0,t.value.length-1)+'ž';}
 	else if(tVal[tVal.length-1]=='S'){t.value = t.value.substring(0,t.value.length-1)+'š';}
 }
