cf68a315f4ed8c592f7bc5067c70521437fc3be2
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-09-08 17:51:05 +0700

    Update ipbipona.js

--- ipbipona/ipbipona.js
+++ ipbipona/ipbipona.js
@@ -4,7 +4,6 @@ function draw(){
 	tVal = t.value;
 	if(tVal.substring(tVal.length-2,tVal.length)=='g\;'){t.value = t.value.substring(0,t.value.length-2)+'ǧ';}
 	else if(tVal[tVal.length-1]=='G'){t.value = t.value.substring(0,t.value.length-1)+'ɢ';}
-	else if(tVal[tVal.length-1]=='J'){t.value = t.value.substring(0,t.value.length-1)+'ǰ';}
 	else if(tVal[tVal.length-1]=='C'){t.value = t.value.substring(0,t.value.length-1)+'č';}
 	else if(tVal[tVal.length-1]=='K'){t.value = t.value.substring(0,t.value.length-1)+'ᴋ';}
 	else if(tVal[tVal.length-1]=='N'){t.value = t.value.substring(0,t.value.length-1)+'ñ';}
@@ -20,8 +19,7 @@ function draw(){
 	else if(tVal[tVal.length-1]=='B'){t.value = t.value.substring(0,t.value.length-1)+'ʙ';}
 	else if(tVal[tVal.length-1]=='P'){t.value = t.value.substring(0,t.value.length-1)+'ᴘ';}
 	else if(tVal.substring(tVal.length-2,tVal.length)=='p\;'){t.value = t.value.substring(0,t.value.length-2)+'ṕ';}
-	else if(tVal[tVal.length-1]=='V'){t.value = t.value.substring(0,t.value.length-1)+'ʌ';}
-	else if(tVal[tVal.length-1]=='F'){t.value = t.value.substring(0,t.value.length-1)+'ꜰ';}
+	else if(tVal[tVal.length-1]=='W'){t.value = t.value.substring(0,t.value.length-1)+'ʍ';}
 	else if(tVal[tVal.length-1]=='Z'){t.value = t.value.substring(0,t.value.length-1)+'ž';}
 	else if(tVal[tVal.length-1]=='S'){t.value = t.value.substring(0,t.value.length-1)+'š';}
 }
